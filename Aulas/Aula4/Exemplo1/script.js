31c85d81a0ad61f699b3e9af82694f122ceef223
pamelladsco <pamellacarraro@hotmail.com>
2025-09-23 19:43:11 -0300

    Adicionado a pasta aulas

---
+++ Aulas/Aula4/Exemplo1/script.js
@@ -0,0 +1,17 @@
+/* Exemplo 1 */
+
+/* Pamella dos Santos */
+//comentário de 1 linha
+
+console.log("Olá Mundo")
+console.log("Olá turma 25-2n")
+
+prompt("Qual é o seu nome?")
+
+console.log("Testando novas mensagens")
+
+
+const nome = "Pamella"
+const idade = 27
+
+console.log("O nome é:", nome, "e sua idade é:", idade)
---
+++ Aulas/Aula4/Exercício.js/script.js
@@ -0,0 +1,9 @@
+const nome = "Pamella"
+const sobrenome = "Carraro"
+const idade = "27"
+let estudante = true 
+let naoEstudante = false 
+
+let estuda = (estudante == true) || (naoEstudante = false)
+
+console.log(nome, sobrenome, idade, "É estudante?:", true)
