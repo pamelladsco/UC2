d00490d38398d92caa4158736de32ed9d03e1602
pamelladsco <pamellacarraro@hotmail.com>
2025-09-24 21:51:02 -0300

    Adicionado e alterados arquivos da aula 6

---
+++ Aulas/Aula6/Exercícios/Exercício1.js
@@ -0,0 +1,45 @@
+
+/*let a = 10
+let b = 10
+
+console.log(b)
+
+b = 5
+
+console.log(a,b)
+
+ Let é uma variável que pode mudar, "a" é o nome da nossa variável e 10 é o número atribuido a variável. */
+/* Let é uma váriavel que pode mudar, "b" é o nome da nossa variável e 10 é o número atribuido a variável*/
+/* console.log(b) está imprimindo a variável b que é 10, e mudando a variável de 10 para 5*/
+/* após, estamos imprimindo as novas variáveis que são a = 10 e b = 5 
+
+let a = 10 
+let b = 20 
+c = a
+b = c
+a = b
+
+ A variável let a é 10 */
+/* A variável let b é 20 */
+/* A variável c é igual a, ou seja, 10 */
+/* A variável b é igual c, e se c é igual a, então a variável b que era 20 agora é 10 */
+/* A variável a tem valor 10 e a variável b tem novo valor 10, assim como c*/
+/* Todas as variáveis são 10
+
+console.log(a, b, c)
+
+let horasTrabalha = prompt("Quantas horas você trabalha por dia?") 
+let seuSalario = prompt("Quanto você recebe por dia?")
+console.log("Você recebe", seuSalario, "t", "Você trabalha", horasTrabalha, "por hora")
+
+/* Let horasTrabalha é uma variável que pergunta ao usuário quantas horas por dia ele trabalha/*
+/* Let SeuSalario é uma variável que pergunta ao usuário quanto ele recebe por dia/*
+/* Console.log é para declarar o salário/hora do usuário/*
+/*Nesse código queremos saber qual o valor do salário do usuário e quantas horas ele trabalha por dia
+afim de saber qual o valor da hora trabalhada*/
+
+
+let nome 
+let idade 
+console.log(typeof nome)
+console.log(typeof idade)
